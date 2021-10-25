ee34d784d06a48c48c6f74de97f18df8dab80b07
Bruno <bruno.3av@gmail.com>
2021-10-25 08:51:59 -0300

    Cypress added

---
+++ cypress/integration/1-getting-started/todo.spec.ts
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+import cypress from "cypress";
+
+describe("suite de testes ", () => {
+  cy.visit(
+    "https://globalsqa.com/angularJs-protractor/registration-login-example/#/login"
+  );
+});
---
+++ cypress/integration/aula-inatel/global.spec.ts
@@ -0,0 +1,8 @@
+
+// import describe from "cypress";
+
+// describe("suite de testes ", () => {
+//   cy.visit(
+//     "https://globalsqa.com/angularJs-protractor/registration-login-example/#/login"
+//   );
+// });
